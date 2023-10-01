24b4ddc0b7e7f5f788418fe448e8cf652825fe76
abc6xyz <byte11101100@gmail.com>
2023-09-30 21:52:49 -0700

    port changed

--- server.py
+++ server.py
@@ -14,7 +14,7 @@ async def handle_client(websocket, path):
         except websockets.exceptions.ConnectionClosedError:
             break
 
-start_server = websockets.serve(handle_client, 'localhost', 1234)
+start_server = websockets.serve(handle_client, 'localhost', 80)
 
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
